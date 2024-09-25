522bc8592058d91a36876a1c75c8afbc180ad7fc
Abdulteef1998 <abdulte97@gmail.com>
2024-09-26 00:34:01 +0300

    best seller item part 3

--- lib/Features/splash/presentation/views/widgets/home_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:bookly/Features/splash/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/constants.dart';
-import 'package:bookly/core/utils/assest.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import 'best_seller_list_view_item.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -29,48 +29,6 @@ class HomeViewBody extends StatelessWidget {
             height: 20,
           ),
           BestSalerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSalerListViewItem extends StatelessWidget {
-  const BestSalerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.green,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AssetsData.testImage),
-                  )),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Hary potter and the Gobblet of Fire ',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
