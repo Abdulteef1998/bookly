5b27908b9b8b2a861871b3aecf7a546f5338e709
Abdulteef1998 <abdulte97@gmail.com>
2024-09-26 01:28:03 +0300

    best seller item done

--- lib/Features/splash/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/splash/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/splash/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assest.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -27,32 +28,37 @@ class BestSalerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Hary potter and the Gobblet of Fire ',
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                  maxLines: 2,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    'Hary potter and the Gobblet of Fire ',
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                    maxLines: 2,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text('J.K.Rowling', style: Styles.textStyle14),
-              const SizedBox(
-                height: 3,
-              ),
-              Row(
-                children: [
-                  Text('19.99 €',
-                      style: Styles.textStyle20
-                          .copyWith(fontWeight: FontWeight.bold)),
-                ],
-              )
-            ],
+                const SizedBox(
+                  height: 3,
+                ),
+                Text('J.K.Rowling', style: Styles.textStyle14),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text('19.99 €',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    const Spacer(),
+                    const BookRating(),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
