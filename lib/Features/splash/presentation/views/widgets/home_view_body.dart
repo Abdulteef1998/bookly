e1c2c6b4f2ad4c7c51e8f096720bde0f4690c328
Abdulteef1998 <abdulte97@gmail.com>
2024-09-26 00:17:01 +0300

    best seller item part 2

--- lib/Features/splash/presentation/views/widgets/home_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/splash/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assest.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -21,8 +22,11 @@ class HomeViewBody extends StatelessWidget {
             height: 50,
           ),
           Text(
-            'Best Saler',
-            style: Styles.textStyle20,
+            'Best Seller',
+            style: Styles.textStyle30.copyWith(fontFamily: kGtSectraFine),
+          ),
+          const SizedBox(
+            height: 20,
           ),
           BestSalerListViewItem(),
         ],
@@ -51,6 +55,21 @@ class BestSalerListViewItem extends StatelessWidget {
                     image: AssetImage(AssetsData.testImage),
                   )),
             ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Hary potter and the Gobblet of Fire ',
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                ),
+              ),
+            ],
           )
         ],
       ),
