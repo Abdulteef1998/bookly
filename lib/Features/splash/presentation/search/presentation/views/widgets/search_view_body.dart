2d91ffc84cd24cc11151f00492e1bc387f7737e3
Abdulteef1998 <abdulte97@gmail.com>
2024-09-30 13:37:50 +0300

    search view done

--- lib/Features/splash/presentation/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/splash/presentation/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,8 +12,41 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: const [CustomSearchTextField()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomSearchTextField(),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: SearchResultListView(),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
