9bf5fef8e203e498d7f72e42f26957235fb09282
AbdulteefRashid <abdulte97@gmail.com>
2024-11-21 23:48:22 +0300

    Create BookModel

--- lib/Features/splash/presentation/home/domain/use_cases/featch_newest_books_use_case.dart
+++ lib/Features/splash/presentation/home/domain/use_cases/featch_newest_books_use_case.dart
@@ -1,9 +1,9 @@
 import 'package:bookly/Features/splash/presentation/home/domain/entity/book_entity.dart';
 import 'package:bookly/Features/splash/presentation/home/domain/repos/home_repo.dart';
-import 'package:bookly/core/use_cases/no_param_use_case.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../../../core/errors/failure.dart';
+import '../../../../../../core/use_cases/no_param_use_case.dart';
 
 class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
--- lib/Features/splash/presentation/home/domain/use_cases/fetch_featured_books.dart
+++ lib/Features/splash/presentation/home/domain/use_cases/fetch_featured_books.dart
@@ -1,8 +1,9 @@
 import 'package:bookly/Features/splash/presentation/home/domain/entity/book_entity.dart';
 import 'package:bookly/Features/splash/presentation/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
-import 'package:bookly/core/use_cases/no_param_use_case.dart';
 import 'package:dartz/dartz.dart';
+
+import '../../../../../../core/use_cases/no_param_use_case.dart';
 
 class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
