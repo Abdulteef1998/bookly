97937f8b61b602834aad80061f4c0b96785d09c7
AbdulteefRashid <abdulte97@gmail.com>
2024-11-24 02:19:27 +0300

    build runner hive

--- lib/Features/splash/presentation/home/domain/entity/book_entity.dart
+++ lib/Features/splash/presentation/home/domain/entity/book_entity.dart
@@ -1,11 +1,21 @@
+import 'package:hive/hive.dart';
+
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
   final String bookId;
+  @HiveField(0)
   final String? image;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String? authorName;
+  @HiveField(3)
   final num? price;
+  @HiveField(4)
   final num? rating;
-
+  @HiveField(5)
   BookEntity(
       {required this.image,
       required this.title,
