84edd19872020cfcaaf87b011e1b44a6fbb04b1c
AbdulteefRashid <abdulte97@gmail.com>
2024-11-27 02:53:15 +0300

    create Server Failure from Dio Error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/Features/splash/presentation/home/domain/entity/book_enti
 import 'package:bookly/Features/splash/presentation/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 import '../data_sources/home_local_data_source.dart';
 
@@ -15,30 +16,40 @@ class HomeRepoImpl extends HomeRepo {
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
-      var booksList = homeLocalDataSource.fetchFeaturedBooks();
-      if (booksList.isNotEmpty) {
-        return right(booksList);
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var books = await homeRemoteDataSorce.fetchFeaturedBooks();
+      books = await homeRemoteDataSorce.fetchFeaturedBooks();
 
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
   }
 
   @override
   Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
+      var books;
       var booksList = homeLocalDataSource.fetchNewestBooks();
       if (booksList.isNotEmpty) {
         return right(booksList);
       }
-      var books = await homeRemoteDataSorce.fetchNewestBooks();
+      books = await homeRemoteDataSorce.fetchNewestBooks();
 
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,50 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioError e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(
+            "Connection timed out. Please check your internet connection.");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure("Send request timed out. Please try again later.");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure("Receive data timed out. Please try again later.");
+      case DioExceptionType.badCertificate:
+        return ServerFailure(
+            "Invalid certificate. Please check your security settings.");
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            e.response!.statusCode, e.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure("Request was cancelled.");
+      case DioExceptionType.connectionError:
+        return ServerFailure(
+            "Connection error. Please check your internet connection.");
+      case DioExceptionType.unknown:
+        return ServerFailure(
+            "An unknown error occurred. Please try again later.");
+    }
+  }
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure(
+          'Your request was not found, please try again later.');
+    } else if (statusCode == 500) {
+      return ServerFailure(
+          'There is a problem with server  , please try again later.');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else {
+      return ServerFailure('There was an error , please try again later');
+    }
+  }
+}
